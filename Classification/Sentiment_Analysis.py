01eb3156bf7464ba7395b5a435eb1f3f67b3e28d
Dr Von Martinez <bknigh1@ilstu.edu>
2022-01-02 23:52:04 -0500

    Created Sentiment_Analysis.py

---
+++ Classification/Sentiment_Analysis.py
@@ -0,0 +1,8 @@
+# Simple Task
+#   Is attitude of the text positive or negative?
+# More Complex:
+#   Rank attitude of text from 1 to 5
+# Advanced:
+# Detect the target, source, or complex attitude types
+
+
